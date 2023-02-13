77dab7816c97e230396f96252bee99404b87c0f9
pigeon-phobia <jbq3712@gmail.com>
2023-02-14 00:00:25 +0800

    add new function to visualize dataset as wordcloud

--- utils/plotutils.py
+++ utils/plotutils.py
@@ -1,4 +1,7 @@
+from utils import preprocess
+
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 sub_a_label_mapping = {'NOT': 'Not Offensive', 'OFF': 'Offensive'}
 sub_b_label_mapping = {'TIN': 'Targeted Insult', 'UNT': 'Untargeted'}
@@ -42,4 +45,23 @@ def plot_tweet_token_length_hist(tweet_list):
     len_tweets = [len(tweet) for tweet in tweet_list]
     plt.hist(len_tweets, bins=30)
     plt.title('Number of tokens')
+    plt.show()
+
+    
+def plot_wordcloud(title, subtitles, *data_list):
+    wc = WordCloud(width=1000, height=500, background_color="white", max_words=200, contour_width=0, random_state=5246)
+    fig, ax = plt.subplots(len(data_list))
+    if title: fig.suptitle(title)
+    for i in range(len(data_list)):
+        tweet_list = data_list[1]
+        tweet_matrix, tweet_vocab = preprocess.count_vectorize(preprocess.join_as_sentence(tweet_list))
+        word_freqs = dict(zip(tweet_vocab, tweet_matrix.A.sum(axis=0)))
+
+        wc.generate_from_frequencies(word_freqs)
+
+        ax[i].imshow(wc, interpolation="bilinear")
+        ax[i].set_title(subtitles[i])
+        ax[i].axis("off")
+    
+    plt.tight_layout()
     plt.show()
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,5 +1,41 @@
 from nltk.tokenize import TweetTokenizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
 
 def nltk_tokenize(tweet_list):
     tweet_tokenizer = TweetTokenizer()
     return [tweet_tokenizer.tokenize(tweet) for tweet in tweet_list]
+
+
+def spacy_pipeline(tweet_list):
+    return [nlp(tweet) for tweet in tweet_list]
+
+
+def spacy_normalize(doc_list, stop_removal=True, lemmatized=True):
+    res = []
+    key = "lemma_" if lemmatized else "text"
+    to_remove = lambda t: t.is_stop if stop_removal else False
+    for doc in doc_list:
+        res.append([getattr(t, key).lower() for t in doc if (not to_remove(t) and t.pos_ not in ['PUNCT'])])
+    return res
+
+
+def join_as_sentence(tweet_list):
+    return [" ".join(tweet) for tweet in tweet_list]
+
+
+def count_vectorize(documents):
+    count_vectorizer = CountVectorizer()
+    X_tf = count_vectorizer.fit_transform(documents)
+    return X_tf, count_vectorizer.get_feature_names_out()
+
+
+def remove_user_mask(documents):
+    res = []
+    for doc in documents:
+        res.append([t for t in doc if t != "@user"])
+    return res
