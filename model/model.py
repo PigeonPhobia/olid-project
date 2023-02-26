036743d67d1252385ff8dda3e42629bf32f33578
pigeon-phobia <jbq3712@gmail.com>
2023-02-26 19:05:02 +0800

    update cnn model to use conv2d and maxpool2d

--- model/model.py
+++ model/model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 class OlidCnnNet(nn.Module):
 
-    def __init__(self, seq_len, num_vocab, embedding_size=64, conv_out=32, threshold=0.5):
+    def __init__(self, seq_len, num_vocab, embedding_size=64, conv_out=2, threshold=0.5):
         super().__init__()
         self.threshold = threshold
 
@@ -12,17 +12,17 @@ class OlidCnnNet(nn.Module):
         self.embedding = nn.Embedding(num_vocab, embedding_size, padding_idx=0)
       
         # Convolution layers definition
-        self.conv_1 = nn.Conv1d(embedding_size, conv_out, 2)
-        #self.conv_2 = nn.Conv1d(embedding_size, conv_out, 3)
-        #self.conv_3 = nn.Conv1d(embedding_size, conv_out, 4)
+        self.conv_1 = nn.Conv2d(1, conv_out, (2, embedding_size))
+        self.conv_2 = nn.Conv2d(1, conv_out, (3, embedding_size))
+        self.conv_3 = nn.Conv2d(1, conv_out, (4, embedding_size))
       
         # Max pooling layers definition
-        self.pool_1 = nn.MaxPool1d(2)
-        #self.pool_2 = nn.MaxPool1d(3)
-        #self.pool_3 = nn.MaxPool1d(4)
+        self.pool_1 = nn.MaxPool2d((1, 2))
+        self.pool_2 = nn.MaxPool2d((1, 3))
+        self.pool_3 = nn.MaxPool2d((1, 4))
         
         # dropout
-        self.dropout = nn.Dropout(0.2)
+        self.dropout = nn.Dropout(0.5)
         
         # find fc layer input size
         test = torch.randint(10, (1, seq_len))
@@ -33,26 +33,25 @@ class OlidCnnNet(nn.Module):
         
     def conv_layers(self, x):
 
-        x = self.embedding(x)
-        x = x.transpose(1, 2)
+        x = self.embedding(x).unsqueeze(1)
       
         # Convolution layer 1 is applied
-        x1 = self.conv_1(x)
+        x1 = self.conv_1(x).squeeze(-1)
         x1 = F.relu(x1)
         x1 = self.pool_1(x1)
       
         # Convolution layer 2 is applied
-        #x2 = self.conv_2(x)
-        #x2 = F.relu(x2)
-        #x2 = self.pool_2(x2)
+        x2 = self.conv_2(x).squeeze(-1)
+        x2 = F.relu(x2)
+        x2 = self.pool_1(x2)
    
         # Convolution layer 3 is applied
-        #x3 = self.conv_3(x)
-        #x3 = F.relu(x3)
-        #x3 = self.pool_3(x3)
+        x3 = self.conv_3(x).squeeze(-1)
+        x3 = F.relu(x3)
+        x3 = self.pool_1(x3)
       
         # The output of each convolutional layer is concatenated into a unique vector
-        union = x1#torch.cat((x1, x2), 2)
+        union = torch.cat((x1, x2, x3), 2)
         union = union.reshape(union.size(0), -1)
         return union
 
